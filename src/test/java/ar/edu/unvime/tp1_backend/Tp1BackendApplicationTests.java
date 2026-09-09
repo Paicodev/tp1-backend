31c2da7048c2370120e7ee616c84e6762a573375
Paicodev <paedmaag@gmail.com>
2026-09-09 11:32:19 -0300

    chore: setup inicial del proyecto springboot con maven

---
+++ src/test/java/ar/edu/unvime/tp1_backend/Tp1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unvime.tp1_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
